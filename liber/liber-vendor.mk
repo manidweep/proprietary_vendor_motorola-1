0670b7af27d4fbad32a8f7e359ef6b7f7708b774
menorziin <williamsilvapppl@gmail.com>
2021-10-28 23:21:10 +0000

    liber: add missing thermal engine
    Change-Id: I12a414524b7c238a741a246c65b2041a3feae964

--- liber/liber-vendor.mk
+++ liber/liber-vendor.mk
@@ -91,6 +91,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/liber/proprietary/vendor/etc/sensors/config/stk3a5x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/stk3a5x_0.json \
     vendor/motorola/liber/proprietary/vendor/etc/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     vendor/motorola/liber/proprietary/vendor/etc/sensors/sns_reg_config:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sns_reg_config \
+    vendor/motorola/liber/proprietary/vendor/etc/thermal-engine-liber.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-liber.conf \
     vendor/motorola/liber/proprietary/vendor/etc/vhw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/vhw.xml \
     vendor/motorola/liber/proprietary/vendor/firmware/AbovCapSense.BIN:$(TARGET_COPY_OUT_VENDOR)/firmware/AbovCapSense.BIN \
     vendor/motorola/liber/proprietary/vendor/firmware/CAMERA_ICP.elf:$(TARGET_COPY_OUT_VENDOR)/firmware/CAMERA_ICP.elf \
